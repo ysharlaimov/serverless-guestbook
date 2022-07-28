e77d089e827635d75147c92a2307d8569d092e54
yevhenii sharlaimov <yevhenii.sharlaimov@gmail.com>
2022-07-28 18:57:23 +0000

    added api url

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://03a4689f.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
